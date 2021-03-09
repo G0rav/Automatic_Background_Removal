73cff99c156adc37c356002f6473bac23468e523
Gaurav <g.jain86078@gmail.com>
2021-03-09 23:55:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,150 @@
+import io as IO
+import os
+import cv2
+import numpy as np
+from skimage import io
+import tensorflow as tf 
+import matplotlib.pyplot as plt
+from flask import Flask, render_template, request, Response
+
+upload_folder = "static"
+app = Flask(__name__)
+
+model = tf.keras.models.load_model('saved-model-02.h5')
+
+
+@app.route("/", methods= ['POST', 'GET'])
+def upload_file():
+    
+    if request.method == "POST":
+        image_file = request.files["image"]
+        URL = request.form["url"]
+        if image_file:
+            image_location = os.path.join(upload_folder, image_file.filename)
+            image_file.save(image_location)
+
+            new_image = load_img(image_location)
+            new_image_name = 'new_'+image_file.filename
+            new_image_location = save_img(new_image, new_image_name)
+
+            pred = prediction(new_image_location)
+            pred = cv2.resize(pred, (256,256))
+            pred_image_name = 'pred_'+image_file.filename
+            pred_location = save_img(pred, pred_image_name)            
+
+            return render_template("index.html", message='Prediction Ready.',
+                                    image_name = new_image_name, pred_image_name= pred_image_name)
+         
+        elif URL:
+            #URL = request.form["url"] 
+            downloaded_img = load_img(URL)
+            downloaded_img_name = URL[-10:-6]+'.jpg'
+            downloaded_img_location = save_img(downloaded_img, downloaded_img_name)
+
+            pred = prediction(downloaded_img_location)
+            pred = cv2.resize(pred, (256,256))
+            pred_image_name = 'pred_'+ downloaded_img_name
+            pred_location = save_img(pred, pred_image_name)
+            return render_template("index.html", message= 'Prediction Ready.',
+                                    image_name = downloaded_img_name, pred_image_name= pred_image_name)
+
+        else:
+            return render_template("index.html", message='Please upload an image or Enter image url.',
+                                image_name = None, pred_image_name= None)
+
+    else:
+        return render_template("index.html", message='Upload an image or Enter image url.',
+                                image_name = None, pred_image_name= None)
+    
+
+
+@app.route('/live-stream', methods=["GET","POST"])
+def index():
+    return render_template('live_stream.html', toggle=None)
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(
+        gen(),
+        mimetype='multipart/x-mixed-replace; boundary=frame'
+    )
+
+
+def load_img(image_location):
+    img = io.imread(image_location)
+    #img = cv2.resize(img[:,:,0:3], (128,128), interpolation=cv2.INTER_AREA)
+    return img
+
+def save_img(img,image_name):
+    """save the image and return the location"""
+    image_location = os.path.join(upload_folder, image_name)
+    plt.imsave(image_location, img)
+    return image_location
+
+
+def prediction(imgpath):
+    im = io.imread(imgpath)
+    im = cv2.resize(im[:,:,0:3],(128,128))
+    
+    img = np.array(im)/255
+    img = img.reshape((1,)+img.shape)
+    pred = model.predict(img)
+
+    p = pred.copy()
+    p = p.reshape(p.shape[1:-1])
+
+    p[np.where(p>.4)] = 1
+    p[np.where(p<.4)] = 0
+
+    im[:,:,0] = im[:,:,0]*p 
+    im[:,:,0][np.where(p!=1)] = 255
+    im[:,:,1] = im[:,:,1]*p 
+    im[:,:,1][np.where(p!=1)] = 255
+    im[:,:,2] = im[:,:,2]*p
+    im[:,:,2][np.where(p!=1)] = 255
+
+    return im
+
+vc = cv2.VideoCapture(0)
+
+def gen():
+    """Video streaming generator function."""
+    while True:
+        read_return_code, frame = vc.read()
+        frame = cv2.resize(frame, (256, 256), interpolation=cv2.INTER_AREA)
+
+        frame = live_prediction(frame)
+
+        encode_return_code, image_buffer = cv2.imencode('.jpg', frame)
+        io_buf = IO.BytesIO(image_buffer)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + io_buf.read() + b'\r\n')
+
+
+
+def live_prediction(img):
+
+    im = np.array(img)/255
+    im = im.reshape((1,) + im.shape)
+    pred = model.predict(im)
+
+    p = pred.copy()
+    p = p.reshape(p.shape[1:-1])
+
+    p[np.where(p>.4)] = 1
+    p[np.where(p<.4)] = 0
+
+    img[:,:,0] = img[:,:,0]*p 
+    img[:,:,0][np.where(p!=1)] = 255
+    img[:,:,1] = img[:,:,1]*p 
+    img[:,:,1][np.where(p!=1)] = 255
+    img[:,:,2] = img[:,:,2]*p
+    img[:,:,2][np.where(p!=1)] = 255
+
+    return img
+
+
+if __name__ == "__main__":
+    app.run()
